78e8f93961fc7c7eafc7d070829e63f3863197d0
EnergyNick <nsikachin@gmail.com>
2023-02-17 17:28:06 +0500

    Delete previous version

--- CSharpBashInterpreter/Commands/Factory/ICommandFactory.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CSharpBashInterpreter.Commands.Factory;
-
-public interface ICommandFactory<out T> where T : AbstractTerminalCommand
-{
-    public T Instantiate(IEnumerable<string> tokens);
-
-    public bool CanBeParsed(IEnumerable<string> tokens);
-}
