b8593d7b60f0fb56065ffdeb70cc3a7a6c7c67bd
PinochetLab <a.vorobev1@g,nsu.ru>
2023-03-08 22:39:02 +0300

    added tests

---
+++ CSharpBashInterpreter.Tests/CommandTests/LsTests.cs
@@ -0,0 +1,42 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSharpBashInterpreter.Commands.Abstractions;
+using CSharpBashInterpreter.Commands.Basic;
+using CSharpBashInterpreter.Exceptions;
+using CSharpBashInterpreter.Semantics;
+
+namespace CSharpBashInterpreter.Tests.CommandTests
+{
+    public class LsTests
+    {
+        [Fact]
+        public void TestCatOnSingleFile()
+        {
+            var tempFileName = System.IO.Path.GetTempPath();
+            try
+            {
+                File.Create(tempFileName + "/f1.txt");
+                File.Create(tempFileName + "/f2.txt");
+                File.Create(tempFileName + "/f3.txt");
+
+                var catCommandExecutable = new LsCommandExecutable(new[] { "ls", tempFileName });
+                var pipe = new Pipe();
+                using (var writer = new StreamWriter(pipe.Writer.AsStream()))
+                using (var reader = new StreamReader(pipe.Reader.AsStream()))
+                {
+                    catCommandExecutable.OutputStream = writer;
+                    catCommandExecutable.Execute().Result.Should().Be(0);
+                    reader.ReadToEndAsync().Result.Should().Be(testText);
+                }
+            }
+            finally
+            {
+                File.Delete(tempFileName);
+            }
+        }
+    }
+}
