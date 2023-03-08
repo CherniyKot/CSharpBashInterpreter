72d7e03e09385d5b57b92a2704870a7e7a9298b9
PinochetLab <a.vorobev1@g,nsu.ru>
2023-03-09 00:30:23 +0300

    fixed rebase problem

--- CSharpBashInterpreter/Commands/Basic/EchoCommandExecutable.cs
+++ CSharpBashInterpreter/Commands/Basic/EchoCommandExecutable.cs
@@ -7,7 +7,7 @@
 /// sends string to the output stream
 /// </summary>
 
-internal class EchoCommandExecutable : BaseCommandExecutable
+public class EchoCommandExecutable : BaseCommandExecutable
 {
     private const int BufferSize = 256;
     private readonly char[] _buffer = new char[BufferSize];
@@ -27,8 +27,8 @@ internal class EchoCommandExecutable : BaseCommandExecutable
             }
             catch (Exception e)
             {
-                await OutputStream.WriteLineAsync(e.Message);
-                await OutputStream.FlushAsync();
+                await ErrorStream.WriteLineAsync(e.Message);
+                await ErrorStream.FlushAsync();
                 return 1;
             }
         }
