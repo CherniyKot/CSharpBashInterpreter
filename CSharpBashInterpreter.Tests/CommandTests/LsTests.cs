c5323b347fb1a12e4a5312146b51861dc6d737d1
PinochetLab <a.vorobev1@g,nsu.ru>
2023-03-08 23:05:10 +0300

    added tests

--- CSharpBashInterpreter.Tests/CommandTests/LsTests.cs
+++ CSharpBashInterpreter.Tests/CommandTests/LsTests.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO.Pipelines;
 using CSharpBashInterpreter.Commands.Abstractions;
 using CSharpBashInterpreter.Commands.Basic;
 using CSharpBashInterpreter.Exceptions;
@@ -14,28 +15,31 @@ namespace CSharpBashInterpreter.Tests.CommandTests
     public class LsTests
     {
         [Fact]
-        public void TestCatOnSingleFile()
+        public void TestLs()
         {
-            var tempFileName = System.IO.Path.GetTempPath();
+            var tempFileName = System.IO.Directory.GetCurrentDirectory();
             try
             {
-                File.Create(tempFileName + "/f1.txt");
-                File.Create(tempFileName + "/f2.txt");
-                File.Create(tempFileName + "/f3.txt");
+                var testText = "";
 
-                var catCommandExecutable = new LsCommandExecutable(new[] { "ls", tempFileName });
+                foreach (var file in Directory.GetFiles(tempFileName))
+                {
+                    testText += Path.GetFileName(file) + '\n';
+                }
+
+                var lsCommandExecutable = new LsCommandExecutable(new[] { "ls" });
                 var pipe = new Pipe();
                 using (var writer = new StreamWriter(pipe.Writer.AsStream()))
                 using (var reader = new StreamReader(pipe.Reader.AsStream()))
                 {
-                    catCommandExecutable.OutputStream = writer;
-                    catCommandExecutable.Execute().Result.Should().Be(0);
+                    lsCommandExecutable.OutputStream = writer;
+                    lsCommandExecutable.Execute().Result.Should().Be(0);
                     reader.ReadToEndAsync().Result.Should().Be(testText);
                 }
             }
             finally
             {
-                File.Delete(tempFileName);
+                
             }
         }
     }
