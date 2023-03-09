4196691f9e9508f99225a57b97eb9be60fdc537f
EnergyNick <nsikachin@gmail.com>
2023-03-09 12:10:05 +0300

    Review fixes

--- CSharpBashInterpreter/Commands/Basic/ExitCommandExecutable.cs
+++ CSharpBashInterpreter/Commands/Basic/ExitCommandExecutable.cs
@@ -9,9 +9,9 @@ public class ExitCommandExecutable : BaseCommandExecutable
     public ExitCommandExecutable(IEnumerable<string> tokens) : base(tokens)
     { }
 
-    public override async Task<int> Execute()
+    public override Task<int> Execute()
     {
         Environment.Exit(Tokens.Length > 1 ? int.Parse(Tokens[1]) : 0);
-        return 0;
+        return Task.FromResult(0);
     }
 }
