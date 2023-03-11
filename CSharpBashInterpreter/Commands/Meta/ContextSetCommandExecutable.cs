b02f9f360f9ec4d8ed9e99864b72bcea8304389f
EnergyNick <nsikachin@gmail.com>
2023-03-11 15:42:15 +0300

    Added meta command for set Environment variable

---
+++ CSharpBashInterpreter/Commands/Meta/ContextSetCommandExecutable.cs
@@ -0,0 +1,19 @@
+﻿using CSharpBashInterpreter.Semantics;
+
+namespace CSharpBashInterpreter.Commands.Meta;
+
+public class ContextSetCommandExecutable : BaseCommandExecutable
+{
+    private readonly IContext _context;
+
+    public ContextSetCommandExecutable(IEnumerable<string> tokens, IContext context) : base(tokens)
+    {
+        _context = context;
+    }
+
+    protected override Task<int> ExecuteInternalAsync()
+    {
+        _context.EnvironmentVariables[Tokens[0]] = Tokens[2];
+        return Task.FromResult(0);
+    }
+}
---
+++ CSharpBashInterpreter/Commands/Meta/ContextSetCommandRepresentation.cs
@@ -0,0 +1,16 @@
+﻿using CSharpBashInterpreter.Commands.Abstractions;
+using CSharpBashInterpreter.Semantics;
+
+namespace CSharpBashInterpreter.Commands.Meta;
+
+public class ContextSetCommandRepresentation : IMetaCommandRepresentation
+{
+    public bool CanBeParsed(IEnumerable<string> data)
+    {
+        var enumerable = data as string[] ?? data.ToArray();
+        return enumerable is [_, "=", _];
+    }
+
+    public ICommandExecutable Build(string[] input, IContext context, ICommandParser parser) =>
+        new ContextSetCommandExecutable(input, context);
+}
